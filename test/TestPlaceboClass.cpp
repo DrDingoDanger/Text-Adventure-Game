8ad05cbd2226a09fc1ca81756db3c635563e0052
wilsonn <wilsonn@mailhost.cs.uleth.ca>
2025-10-10 09:29:10 -0600

    Add unit test for the PlaceboClass

---
+++ test/TestPlaceboClass.cpp
@@ -0,0 +1,14 @@
+/**
+ * @author L Nicole Wilson [n.wilson@uleth.ca]
+ * @date 2025-10
+ */
+
+ #include "PlaceboClass.hpp"
+
+ TEST(TestPlaceboClass, defaultConstructorTest) {
+    PlaceboClass* pc = new PlaceboClass();
+
+    EXPECT_NE(pc, nullptr);
+
+    delete pc;
+ }
