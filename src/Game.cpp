77bc14676fa5bcc58017b29a74a3bc602fb764a3
CJ Davidson <curt.davidson@uleth.ca>
2026-03-31 13:12:37 -0600

    added win/loss condition into game and player for when you get emerald and lose when you lose health or starve

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Game.hpp"
-Game::Game() : _player(_map.getLocation(0)) {}
+
+Game::Game() : _player(_map.getLocation(0)), _winItem("Emerald") {}
 
 bool Game::isOver() const {
     return _isOver;
@@ -20,7 +21,6 @@ void Game::update() {
     int action = _ui.playerAction();
 
     if (action == 0) {
-        //MOVEMENT
         Location* loc = _player.getCurrent();
         _display.drawMap(std::cout, &_map, loc);
         std::string direction = _ui.moveDirection();
@@ -31,36 +31,30 @@ void Game::update() {
             direction = _ui.moveDirection();
             _player.moveAction(direction, &_map, valid);
         }
-        //REMOVE HUNGER
         _player.eat(-5);
-
     } else if (action == 1) {
-        //display inventory
         _display.displayInventory(std::cout, _player.getInventory());
         _display.displayPlayerStats(std::cout, _player);
-        //use item
         std::cout << "Would you like to use an item?\n";
         if (_player.inventorySize() > 0) {
             char ans = _ui.yesOrNo();
             if (ans == 'y') {
-                //GET ITEM NO USE
                 std::cout << "Enter the items associated number.\n";
-                //CAN ONLY CHOOSE ITEM THAT EXISTS
                 int choice = _ui.limitInput(_player.inventorySize());
                 Item* temp = _player.getItemAt(choice);
                 if (temp->getType() == "material") {
-                    std::cout << "This item cannot be used.";
+                    std::cout << "This item cannot be used.\n";
                 } else if (temp->getType() == "food") {
                     Food* tempF = dynamic_cast<Food*>(temp);
                     int restore = tempF->getHunger();
                     _player.eat(restore);
                     _player.removeItem(tempF);
-                } else if (temp->getType() == "weapon" ) {
+                } else if (temp->getType() == "weapon") {
                     Weapon* tempW = dynamic_cast<Weapon*>(temp);
                     int power = tempW->getPower();
                     if (power > _player.getAttack()) {
-                        _player.setAttack(power); 
-                        _player.removeItem(tempW);  
+                        _player.setAttack(power);
+                        _player.removeItem(tempW);
                     } else {
                         std::cout << "Cannot use item, does not effect attack damage\n";
                     }
@@ -68,14 +62,11 @@ void Game::update() {
             }
         }
     } else if (action == 2) {
-        //DRAW MAP
         Location* loc = _player.getCurrent();
         _display.drawMap(std::cout, &_map, loc);
     } else if (action == 3) {
-        //display NPCs
         Location* loc = _player.getCurrent();
         _display.displayNPC(std::cout, loc);
-        //interact NPC
         std::cout << "Would you like to interact with an NPC?\n";
         char ans = _ui.yesOrNo();
         if (ans == 'y') {
@@ -83,33 +74,29 @@ void Game::update() {
             int choice = _ui.limitInput(loc->numOfNPC());
             NPC* temp = loc->getNPC(choice);
             if (temp->getType() == "help") {
-                //HELPNPC INTERACT
                 HelpNPC* tempH = dynamic_cast<HelpNPC*>(temp);
                 std::cout << '\n' << tempH->giveHint() << '\n';
             } else if (temp->getType() == "shop") {
-                //SHOPNPC INTERACT
                 ShopNPC* tempS = dynamic_cast<ShopNPC*>(temp);
-                Inventory& temp = _player.getInventory();
+                Inventory& tempInv = _player.getInventory();
                 std::vector<CraftingRecipe*>& tempTrades = tempS->getTrades();
-                _display.displayTrades(std::cout, tempTrades, temp);
+                _display.displayTrades(std::cout, tempTrades, tempInv);
                 std::cout << "Would you like to make a trade?\n";
-                char ans = _ui.yesOrNo();
-                if (ans == 'y') {
+                char ans2 = _ui.yesOrNo();
+                if (ans2 == 'y') {
                     std::cout << "Enter the trade's associated number.\n";
                     choice = _ui.limitInput(tempTrades.size());
                     CraftingRecipe* recipe = tempTrades[choice];
-                    recipe->craft(temp);
+                    recipe->craft(tempInv);
                 }
             }
         }
     } else if (action == 4) {
-        //DISPLAY AREA RESOURCES
         Location* loc = _player.getCurrent();
         Inventory& temp = loc->getInventory();
         _display.displayInventory(std::cout, temp);
         std::cout << "Would you like to collect these items?\n";
         char ans = _ui.yesOrNo();
-        //COLLECT RESOURCES
         if (ans == 'y') {
             Inventory& tempP = _player.getInventory();
             while (temp.size() != 0) {
@@ -119,28 +106,36 @@ void Game::update() {
             }
         }
     } else if (action == 5) {
-        //DISPLAY GAME INSTRUCTIONS
         _display.gameInstructions(std::cout);
     } else {
-        //NOT A VALID ACTION CHOICE
-        std::cout << "Invalid input";
+        std::cout << "Invalid input\n";
     }
 
     if (checkWin() || checkLose()) {
         endGame();
     }
 
-    _ui.pressEnter();
+    if (!_isOver) {
+        _ui.pressEnter();
+    }
 }
 
-bool Game::checkWin() const {
-    return false;
+bool Game::checkWin() {
+    return _player.getInventory().hasName(_winItem, 1);
 }
 
 bool Game::checkLose() const {
-    return _player.getHealth() <= 0;
+    return _player.getHealth() <= 0 || _player.getHunger() <= 0;
 }
 
 void Game::endGame() {
+    if (checkWin()) {
+        std::cout << "\nYou win! You found the " << _winItem << ".\n";
+    } else if (_player.getHealth() <= 0) {
+        std::cout << "\nYou lose! Your health reached 0.\n";
+    } else if (_player.getHunger() <= 0) {
+        std::cout << "\nYou lose! Your hunger reached 0.\n";
+    }
+
     _isOver = true;
 }
