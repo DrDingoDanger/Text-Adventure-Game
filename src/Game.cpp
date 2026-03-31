5280c0217af4790d51602a0ac93e1da7dd975792
CJ Davidson <curt.davidson@uleth.ca>
2026-03-31 13:44:13 -0600

    removed comments

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Game.hpp"
+
 Game::Game() : _player(_map.getLocation(0)) {}
 
 bool Game::isOver() const {
@@ -20,7 +21,6 @@ void Game::update() {
     int action = _ui.playerAction();
 
     if (action == 0) {
-        //MOVEMENT
         Location* loc = _player.getCurrent();
         _display.drawMap(std::cout, &_map, loc);
         std::string direction = _ui.moveDirection();
@@ -31,21 +31,16 @@ void Game::update() {
             direction = _ui.moveDirection();
             _player.moveAction(direction, &_map, valid);
         }
-        //REMOVE HUNGER
         _player.eat(-5);
 
     } else if (action == 1) {
-        //display inventory
         _display.displayInventory(std::cout, _player.getInventory());
         _display.displayPlayerStats(std::cout, _player);
-        //use item
         std::cout << "Would you like to use an item?\n";
         if (_player.inventorySize() > 0) {
             char ans = _ui.yesOrNo();
             if (ans == 'y') {
-                //GET ITEM NO USE
                 std::cout << "Enter the items associated number.\n";
-                //CAN ONLY CHOOSE ITEM THAT EXISTS
                 int choice = _ui.limitInput(_player.inventorySize());
                 Item* temp = _player.getItemAt(choice);
                 if (temp->getType() == "material") {
@@ -69,14 +64,11 @@ void Game::update() {
             }
         }
     } else if (action == 2) {
-        //DRAW MAP
         Location* loc = _player.getCurrent();
         _display.drawMap(std::cout, &_map, loc);
     } else if (action == 3) {
-        //display NPCs
         Location* loc = _player.getCurrent();
         _display.displayNPC(std::cout, loc);
-        //interact NPC
         std::cout << "Would you like to interact with an NPC?\n";
         char ans = _ui.yesOrNo();
         if (ans == 'y') {
@@ -84,11 +76,9 @@ void Game::update() {
             int choice = _ui.limitInput(loc->numOfNPC());
             NPC* temp = loc->getNPC(choice);
             if (temp->getType() == "help") {
-                //HELPNPC INTERACT
                 HelpNPC* tempH = dynamic_cast<HelpNPC*>(temp);
                 std::cout << '\n' << tempH->giveHint() << '\n';
             } else if (temp->getType() == "shop") {
-                //SHOPNPC INTERACT
                 ShopNPC* tempS = dynamic_cast<ShopNPC*>(temp);
                 Inventory* invT = _player.getInventory();
                 std::vector<CraftingRecipe*>& tempTrades = tempS->getTrades();
@@ -104,13 +94,11 @@ void Game::update() {
             }
         }
     } else if (action == 4) {
-        //DISPLAY AREA RESOURCES
         Location* loc = _player.getCurrent();
         Inventory* temp = loc->getInventory();
         _display.displayInventory(std::cout, temp);
         std::cout << "Would you like to collect these items?\n";
         char ans = _ui.yesOrNo();
-        //COLLECT RESOURCES
         if (ans == 'y') {
             Inventory* tempP = _player.getInventory();
             while (temp->size() != 0) {
@@ -120,10 +108,8 @@ void Game::update() {
             }
         }
     } else if (action == 5) {
-        //DISPLAY GAME INSTRUCTIONS
         _display.gameInstructions(std::cout);
     } else {
-        //NOT A VALID ACTION CHOICE
         std::cout << "Invalid input";
     }
 
